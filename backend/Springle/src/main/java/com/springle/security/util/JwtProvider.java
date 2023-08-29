e55225ddeda1d468252dc933251c2b9e519ef93f
Shin-sangwon <dnjsml10@gmail.com>
2023-08-30 00:18:26 +0900

    (#3) refactor : jwt token에 pk값도 저장

--- backend/Springle/src/main/java/com/springle/security/util/JwtProvider.java
+++ backend/Springle/src/main/java/com/springle/security/util/JwtProvider.java
@@ -17,6 +17,15 @@ public class JwtProvider {
     private final long accessTokenExpireTimeMs = 3600000L; // 1시간
     private final long refreshTokenExpireTimeMs = 1209600000L; // 2주일
 
+    public static long getId(String token, String secretKey) {
+        return Jwts.parserBuilder()
+                   .setSigningKey(secretKey.getBytes(StandardCharsets.UTF_8))
+                   .build()
+                   .parseClaimsJws(token)
+                   .getBody()
+                   .get("id", long.class);
+    }
+
     public static String getLoginId(String token, String secretKey) {
         return Jwts.parserBuilder()
                    .setSigningKey(secretKey.getBytes(StandardCharsets.UTF_8))
@@ -36,8 +45,9 @@ public class JwtProvider {
                    .before(new Date());
     }
 
-    public String createToken(String loginId, Role role, String secretKey) {
+    public String createToken(Long id, String loginId, Role role, String secretKey) {
         Claims claims = Jwts.claims();
+        claims.put("id", id);
         claims.put("loginId", loginId);
         claims.put("role", role.name());
 
