26a84dd1d26919c0a5c2915957190e55ddda1a88
Shin-sangwon <dnjsml10@gmail.com>
2023-08-29 22:13:36 +0900

    init

---
+++ backend/Springle/src/main/java/com/springle/SpringleApplication.java
@@ -0,0 +1,13 @@
+package com.springle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringleApplication.class, args);
+    }
+
+}
---
+++ backend/Springle/src/test/java/com/springle/SpringleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
