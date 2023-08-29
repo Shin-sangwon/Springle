d3d76f4d6e839a802d327cf93bef18c6b423063e
Shin-sangwon <dnjsml10@gmail.com>
2023-08-30 00:24:15 +0900

    (#3) feat : UserDetailService 구현

--- backend/Springle/src/main/java/com/springle/user/entity/User.java
+++ backend/Springle/src/main/java/com/springle/user/entity/User.java
@@ -1,17 +1,68 @@
 package com.springle.user.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import java.util.Collection;
+import java.util.Collections;
 import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 @Getter
 @Entity(name = "user")
-public class User {
+public class User implements UserDetails {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
+    @Column
+    private String loginId;
+
+    @Column
+    private String loginPassword;
+
+    @Column
+    @Enumerated(EnumType.STRING)
     private Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singletonList(new SimpleGrantedAuthority(getRole().name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return getLoginPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return getLoginId();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
 }
--- backend/Springle/src/main/java/com/springle/user/repository/UserRepository.java
+++ backend/Springle/src/main/java/com/springle/user/repository/UserRepository.java
@@ -1,8 +1,11 @@
 package com.springle.user.repository;
 
 import com.springle.user.entity.User;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByLoginId(String loginId);
 
 }
