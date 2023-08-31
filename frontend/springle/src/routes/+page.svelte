120f8029699cad866ae549b6bab07a3b369e1308
Shin-sangwon <dnjsml10@gmail.com>
2023-09-01 01:22:18 +0900

    (#6) feat : layout 적용

--- frontend/springle/src/routes/+page.svelte
+++ frontend/springle/src/routes/+page.svelte
@@ -2,7 +2,7 @@
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
 <style lang="postcss">
-    :global(html) {
-      background-color: theme(colors.green.100);
-    }
+	:global(html) {
+		background-color: theme(colors.gray.100);
+	}
 </style>
