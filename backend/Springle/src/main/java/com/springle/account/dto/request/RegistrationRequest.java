9a986c5f181089b9d9ca575a0bbac673cde106f8
Shin-sangwon <dnjsml10@gmail.com>
2023-09-03 01:59:22 +0900

    (#5) feat : validation 추가

--- backend/Springle/src/main/java/com/springle/account/dto/request/RegistrationRequest.java
+++ backend/Springle/src/main/java/com/springle/account/dto/request/RegistrationRequest.java
@@ -2,12 +2,14 @@ package com.springle.account.dto.request;
 
 import com.springle.member.entity.Member;
 import com.springle.member.entity.Role;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 
 public record RegistrationRequest(
-    String loginId,
-    String loginPassword,
-    String name,
-    String email
+    @NotBlank String loginId,
+    @NotBlank String loginPassword,
+    @NotBlank String name,
+    @Email String email
 ) {
 
     public Member toEntity(String encodePassword, String encodeEmail) {
