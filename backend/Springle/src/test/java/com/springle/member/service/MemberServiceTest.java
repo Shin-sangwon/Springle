7643c33ef0773d8f2a6263de79b053f14eddfc3e
Shin-sangwon <dnjsml10@gmail.com>
2023-09-03 02:22:19 +0900

    (#5) test : 없는 회원 조회 테스트

--- backend/Springle/src/test/java/com/springle/member/service/MemberServiceTest.java
+++ backend/Springle/src/test/java/com/springle/member/service/MemberServiceTest.java
@@ -1,8 +1,10 @@
 package com.springle.member.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import com.springle.member.entity.Member;
+import com.springle.member.exception.MemberException;
 import com.springle.util.ServiceTest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -20,6 +22,13 @@ class MemberServiceTest extends ServiceTest {
         Member member = memberService.findById(id);
 
         assertThat(member).isNotNull();
+    }
+
+    @DisplayName("존재하지 않는 회원 MemberException 던진다")
+    @Test
+    void findNotExistsMemberThrowsMemberException() throws Exception{
+
+        assertThrows(MemberException.class, () -> memberService.findById(77777L));
     }
 
 }
